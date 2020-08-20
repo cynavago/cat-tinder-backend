efda88ca39c2362f412bee69be411e2f28fb7d66
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2020-08-20 13:06:27 -0700

    Still need to set up CORS

--- cat_tinder_backend/spec/models/cat_spec.rb
+++ cat_tinder_backend/spec/models/cat_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should validate name" do
+    cat = Cat.create
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it "should validate enjoys" do
+    cat = Cat.create
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+  
 end
--- cat_tinder_backend/spec/requests/cats_request_spec.rb
+++ cat_tinder_backend/spec/requests/cats_request_spec.rb
@@ -1,5 +1,87 @@
 require 'rails_helper'
 
-RSpec.describe "Cats", type: :request do
+describe "Cats", type: :request do
+  it "gets a list of Cats" do
+    # Create a new cat in the Test Database (this is not the same one as development)
+    Cat.create(name: 'Felix', age: 2, enjoys: 'Walks in the park', story: 'story', image: 'image')
+
+    # Make a request to the API
+    get '/cats'
+
+    # Convert the response into a Ruby Hash
+    json = JSON.parse(response.body)
+
+    # Assure that we got a successful response
+    expect(response).to have_http_status(:ok)
+
+    # Assure that we got one result back as expected
+    expect(json.length).to eq 1
+  end
+
+  it "creates a cat" do
+    # The params we are going to send with the request
+    cat_params = {
+      cat: {
+        name: 'Buster',
+        age: 4,
+        enjoys: 'Meow Mix, and plenty of sunshine.',
+        story: 'Loves to eat. Hates cucumbers.',
+        image: 'image of Buster'
+      }
+    }
+
+    # Send the request to the server
+    post '/cats', params: cat_params
+
+    # Assure that we get a success back
+    expect(response).to have_http_status(:ok)
+
+    # Look up the cat we expect to be created in the Database
+    cat = Cat.first
+
+    # Assure that the created cat has the correct attributes
+    expect(cat.name).to eq 'Buster'
+  end
+
+  it "doesn't create a cat without a name" do
+    cat_params = {
+      cat: {
+        age: 2,
+        enjoys: 'Walks',
+        story: 'things',
+        image: 'picture'
+      }
+    }
+    # Send the request to the  server
+    post '/cats', params: cat_params
+    # expect an error if the cat_params does not have a name
+    expect(response.status).to eq 422
+    # Convert the JSON response into a Ruby Hash
+    cats = JSON.parse(response.body)
+    # Errors are returned as an array because there could be more than one, if there are more than one validation failures on an attribute.
+    # NOTE: The string that follows .include is NOT arbitrary
+    expect(cats['name']).to include "can't be blank"
+  end
+
+  it "doesn't create a cat that has an enjoys category with less than 10 characters" do
+    cat_params = {
+      cat: {
+        name:'Benedict',
+        age: 2,
+        enjoys: 'Walks',
+        story: 'things',
+        image: 'picture'
+      }
+    }
+    # Send the request to the  server
+    post '/cats', params: cat_params
+    # expect an error if the enjoys is not 10 characters at least
+    expect(response.status).to eq 422
+    # Convert the JSON response into a Ruby Hash
+    cats = JSON.parse(response.body)
+    # Errors are returned as an array because there could be more than one, if there are more than one validation failures on an attribute.
+    # NOTE: The string that follows .include is NOT arbitrary
+    expect(cats['enjoys']).to include "is too short (minimum is 10 characters)"
+  end
 
 end
